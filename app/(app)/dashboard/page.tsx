61509fb2166aa078084745400d2a19a625b7935a
aminhaiqal <aminhaiqal15@gmail.com>
2026-01-14 17:06:35 +0800

    add tanstack form and also geist form

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -8,9 +8,9 @@ export default function Page() {
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight ml-6">
-                Pending Order
-              </h4>
+              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+      Products
+    </h3>
               <DataTable data={data} />
             </div>
           </div>
